712334a0b6851890604ecfa0ecbd90aa2e42ef69
n-d-r-d-g <96367205+n-d-r-d-g@users.noreply.github.com>
2025-07-16 08:37:27 +0400

    test: add detailed session comparison tests

--- cypress/e2e/compare-with-mscc.cy.ts
+++ cypress/e2e/compare-with-mscc.cy.ts
@@ -5,6 +5,7 @@ describe("compare with MSCC", () => {
     cy.visit("https://conference.mscc.mu/agenda");
 
     // Retrieve registration session (same across all conference days)
+    cy.get("#day-thursday + label").click();
     cy.get("div.session__wrapper").should("be.visible");
     cy.get("div.session__wrapper > .tile_start").should(
       "contain.text",
@@ -54,5 +55,95 @@ describe("compare with MSCC", () => {
       sessionsData.find((group) => group.groupName === "Saturday")?.sessions
         .length ?? 0
     );
+  });
+
+  it("checks that MSCC's sessions are the same as DevCon Scheduler", () => {
+    cy.visit("https://conference.mscc.mu/agenda");
+
+    const compareSessionsByDay = (day: string, date: string) => {
+      const sessionsByDay = sessionsData.find(
+        (session) =>
+          session.groupName === `${day.charAt(0).toUpperCase()}${day.slice(1)}`
+      )?.sessions;
+      cy.get(`#agenda-${day} a.session__wrapper`)
+        .filter(
+          (_index, session) => session.querySelector("h3")?.innerText !== "TBA"
+        )
+        .each((session, index) => {
+          const isOpeningKeynote = index === 0;
+          const sessionHTMLElement = session.get(0);
+
+          // Compare id
+          expect(session).to.have.attr(
+            "href",
+            `/agenda/${sessionsByDay?.[index].id}`
+          );
+
+          // Compare title
+          let title = "";
+
+          if (isOpeningKeynote) {
+            const titleParts: Array<string> = [];
+            sessionHTMLElement.querySelectorAll("h3").forEach((el, idx) => {
+              if (idx === 1) {
+                return (titleParts[2] = el.innerText);
+              }
+
+              titleParts.push(el.innerText);
+            });
+            titleParts[1] = `(${sessionHTMLElement
+              .querySelector(".sponsor-logo img")
+              ?.getAttribute("src")
+              ?.split("/")
+              .at(-1)
+              ?.split(".")[0]
+              .split("-")
+              .join("|")})`;
+            title = `${titleParts[0]} ${titleParts[1]}: ${titleParts[2]}`;
+          } else {
+            title = sessionHTMLElement.querySelector("h3")?.innerText ?? "";
+          }
+          expect(title.toLocaleUpperCase()).to.eq(
+            sessionsByDay?.[index].title.toLocaleUpperCase()
+          );
+
+          const sessionScheduleParts =
+            sessionHTMLElement.querySelector("div")?.innerText.split(" - ") ??
+            [];
+
+          // Compare startsAt
+          const sessionStartsAt = `${date}T${sessionScheduleParts[0].trim()}:00`;
+          expect(sessionStartsAt).to.eq(sessionsByDay?.[index].startsAt);
+
+          // Compare endsAt
+          const sessionEndsAt = `${date}T${sessionScheduleParts[1].trim()}:00`;
+          expect(sessionEndsAt).to.eq(sessionsByDay?.[index].endsAt);
+
+          // Compare speakers
+          const sessionSpeakers = [
+            ...sessionHTMLElement.querySelectorAll(".speaker--headshot > div"),
+          ]
+            .map((speakerEl) => (speakerEl as HTMLElement).innerText)
+            .join(", ");
+          expect(sessionSpeakers).to.eq(
+            sessionsByDay?.[index].speakers
+              .map((speaker) => speaker.name)
+              .join(", ")
+          );
+
+          // Compare room
+          expect(session).to.have.attr(
+            "data-room",
+            // There's a typo on MSCC's website - Accelarator instead of Accelerator
+            sessionsByDay?.[index].room === "Accelerator"
+              ? "Accelarator"
+              : sessionsByDay?.[index].room
+          );
+        });
+    };
+
+    compareSessionsByDay("thursday", "2025-07-24");
+    compareSessionsByDay("friday", "2025-07-25");
+    compareSessionsByDay("saturday", "2025-07-26");
   });
 });
